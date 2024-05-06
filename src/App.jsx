3bb8da75a0910577077212f24cc962300c1a9799
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-06 18:32:57 -0500

    Creating login and shopcart pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,3 @@
+import { Login } from "./pages/login/Login"
+import { ShopCart } from "./pages/shop-cart/ShopCart"
 export const App = () => <h1>This works</h1>
---
+++ src/pages/shop-cart/ShopCart.jsx
@@ -0,0 +1,1 @@
+export const ShopCart = () => <h1> Shopcart Page</h1>
