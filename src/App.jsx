8a109e34dce1190c347f66083e2a6389c9a274b0
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-06 18:27:31 -0500

    Creating app element

---
+++ src/App.jsx
@@ -0,0 +1,1 @@
+export const App = () => <h1>This works</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 const root = document.getElementById('root');
-ReactDOM.createRoot(root).render(<h1>Hola Mundo</h1>);
+ReactDOM.createRoot(root).render(<App />);
