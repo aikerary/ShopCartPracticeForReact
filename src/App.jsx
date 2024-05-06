29f3243ce7ff877484d759fb256167c3e937e428
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-06 18:54:39 -0500

    Adding new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import {
-    BrowserRouter,
+    BrowserRouter as Router,
     Routes,
     Route
 } from "react-router-dom";
 
 import { Login } from "./pages/login/Login"
 import { ShopCart } from "./pages/shop-cart/ShopCart"
+import { NotFound } from "./pages/notfound/NotFound"
 
 export const App = () => {
-  return (
-    <div>
-      <h1>App</h1>
-      <Login />
-      <ShopCart />
-    </div>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/shop" element={<ShopCart />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    )
 }
