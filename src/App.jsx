dd005e2be7b60b80f3c923eb4ee6cdf6f8121bb6
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-06 19:17:58 -0500

    Adding login info

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import {
     BrowserRouter as Router,
     Routes,
-    Route
+    Route,
+    useParams,
+    useRouteMatch
 } from "react-router-dom";
 
 import { Login } from "./pages/login/Login"
 import { ShopCart } from "./pages/shop-cart/ShopCart"
 import { NotFound } from "./pages/notfound/NotFound"
+import { useEffect, useState } from "react";
 
 export const App = () => {
+    const [login , setLogin] = useState(false);
+    const {id} = useParams();
+    const match = useRouteMatch();
+    const url = `${match.url}`;
+    // Get the route using
+    useEffect(() => {
+        console.log(url);
+    },[])
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Login info={'info'} />} />
                 <Route path="/shop" element={<ShopCart />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
